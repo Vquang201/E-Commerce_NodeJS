4d9a3f5013f15c5fdd2dda66181fc46021a987c5
Van Quang <vanquanggt01@gmail.com>
2023-09-19 12:28:35 +0700

    fix bugs

--- router/ProductCategory.js
+++ router/ProductCategory.js
@@ -3,7 +3,7 @@ const { verifyToken, isAdmin } = require('../middleware/verifyToken')
 const router = require('express').Router()
 
 router.get('/', ProductCategoryController.getCategoryProducts)
-router.post('/create-category', [verifyToken, isAdmin], ProductCategoryController.createCategoryProduct)
+router.post('/create-category', [verifyToken], ProductCategoryController.createCategoryProduct)
 router.put('/update-category/:id', [verifyToken, isAdmin], ProductCategoryController.updateCategoryProduct)
 router.delete('/delete-category/:id', [verifyToken, isAdmin], ProductCategoryController.deleteCategoryProduct)
 
